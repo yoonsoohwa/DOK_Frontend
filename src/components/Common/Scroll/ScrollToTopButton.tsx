46f06bd37311d8dae70ef50ffee59a89d1a89c8a
yoonsoohwa <blue.yoonsh01@gmail.com>
2023-11-17 06:17:38 +0900

    Design: topButton 디자인 수정

--- src/components/Common/Scroll/ScrollToTopButton.tsx
+++ src/components/Common/Scroll/ScrollToTopButton.tsx
@@ -1,11 +1,11 @@
 import { styled } from "styled-components";
 
 export function ScrollToTopButton() {
-  return <TopButton onClick={() => window.scrollTo(0, 0)} src="/svg/top-button.svg" />;
+  return <TopButton className="pointer" src="/svg/top-button.svg" onClick={() => window.scrollTo(0, 0)} />;
 }
 
 const TopButton = styled.img`
   position: fixed;
-  bottom: 30px;
+  bottom: 70px;
   right: 30px;
 `;
