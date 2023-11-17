8cf619b517050e71c3e5f394853bd8da5c1dc046
[코치] 루카스 <cogito.paradigm@gmail.com>
2023-11-17 15:20:37 +0000

    Revert "Merge branch 'dev' into 'main'"
    This reverts merge request !14

--- src/pages/SignUp.tsx
+++
@@ -1,9 +0,0 @@
-import { styled } from "styled-components";
-import React from "react";
-import { SignUp } from "../components/SignUp/SignUp";
-
-export function SignUpPage() {
-  return (
-    <SignUp />
-  );
-}
