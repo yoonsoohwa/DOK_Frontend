ac14b7dc92cb5f16af3f6a9045fb61f80a54ed7f
yoonsoohwa <blue.yoonsh01@gmail.com>
2023-11-21 05:58:26 +0900

    feat: 매칭상태 radio element 추가

---
+++ src/components/Matching/MatchingStatusRadio.tsx
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+import { Radio, RadioGroup } from "@mui/joy";
+
+
+export function MatchingStatusRadio() {
+  return (
+    <div>
+      <Radio />
+    </div>
+  );
+}
--- src/pages/MatchingList.tsx
+++ src/pages/MatchingList.tsx
@@ -3,6 +3,7 @@ import { MatchingCardList } from "../components/matching/MatchingCardList";
 import { ScrollToTopButton } from "../components/common/ScrollTopButton";
 import { ListPageTopBar } from "../components/common/ListPageTopBar";
 import { MatchingBanner } from "../components/matching/MatchingBanner";
+import { MatchingStatusRadio } from "../components/matching/MatchingStatusRadio";
 
 export function MatchingListPage() {
   return (
@@ -10,6 +11,7 @@ export function MatchingListPage() {
       <MatchingBanner />
       <Section>
         <ListPageTopBar text={["132", "개의 산책 인증이 있습니다."]} />
+        <MatchingStatusRadio />
         <MatchingCardList posts={new Array(16).fill(0)} />
       </Section>
       <ScrollToTopButton />
