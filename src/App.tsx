ab5e3f027ef2dba575ee5fa6cf6c86710c585369
xjqhdjq <xjqhdjq@gmail.com>
2023-11-21 03:18:21 +0900

    Docs: 디렉터리명 수정에 따른 코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import styled, { ThemeProvider } from "styled-components";
 import "./App.css";
 import { theme, defaultTheme } from "./components/styles/theme";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ScrollToTop from "./components/Common/ScrollTopRendering";
-import { NonMemberHeader } from "./components/Header/Header";
-import { MemberHeader } from "./components/Header/Header";
-import { Footer } from "./components/Footer/Footer";
+import ScrollToTop from "./components/common/ScrollTopRendering";
+import { NonMemberHeader } from "./components/header/Header";
+import { MemberHeader } from "./components/header/Header";
+import { Footer } from "./components/footer/Footer";
 
 import {CertificationCreatePage, CertificationListPage,LoginPage, MainPage ,MatchingDetailPage ,
   MatchingListPage ,ProfilePage ,SignUpPage } from "./pages";
