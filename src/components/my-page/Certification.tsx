44573f5b3f6c51d00718772d7707696e13b65cca
xjqhdjq <xjqhdjq@gmail.com>
2023-11-21 09:30:02 +0900

    Feat: 매칭,인증 페이지 완료

--- src/components/my-page/Certification.tsx
+++ src/components/my-page/Certification.tsx
@@ -1,7 +1,44 @@
 import {styled} from "styled-components";
+import { TopBarTitle } from "common/TopBarTitle";
+import { CertifiPostCard } from "../certification/PostCard";
 
 export const Certification = () => {
+    
+    const matchingData = 8;
+    
     return (
-        <div>CertificationTest</div>
+        <MainFrame>
+            <TitleFrame>
+                <TopBarTitle yellow="5" black="개의 산책 인증을 했습니다." />    
+            </TitleFrame>       
+            <CardFrame>
+                <CertifiPostCard onclick={(e: React.MouseEvent): void => {}}/>
+                <CertifiPostCard onclick={(e: React.MouseEvent): void => {}}/>
+                <CertifiPostCard onclick={(e: React.MouseEvent): void => {}}/>
+                <CertifiPostCard onclick={(e: React.MouseEvent): void => {}}/>
+                <CertifiPostCard onclick={(e: React.MouseEvent): void => {}}/>
+            </CardFrame>
+        </MainFrame>
     )
 }
+
+const MainFrame = styled.div`
+    display: flex;
+    flex-direction: column;    
+`
+const TitleFrame = styled.div`
+    display: flex;
+    margin: 5% 0 7% 0;
+    
+`
+const CardFrame = styled.div`
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    
+    & > div {
+        /* border: 5px solid red; */
+        width: 22%;
+        margin: 0 3% 5% 0;
+    }
+`
