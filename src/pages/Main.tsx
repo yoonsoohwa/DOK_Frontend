65f3e8e6c6cc6cf8c5c34bbc1c7a96acc6ef51d3
LeeJinIe <jj074567@gmail.com>
2023-11-22 13:48:37 +0900

    Feat: Redux 설정 및 초기 store 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,16 +1,27 @@
 import { styled } from "styled-components";
-import React from "react";
+import React, { useEffect } from "react";
 import { MainInfo } from "../components/main/MainInfo";
 import { MainSection1 } from "../components/main/MainSection1";
 import { MainSection2 } from "../components/main/MainSection2";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState, setCertificationPosts, setMatchingPosts, setPets } from "src/store";
 
 export function MainPage() {
+  const { pets, matchingPosts, certificationPosts } = useSelector((state: RootState) => state.main);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(setPets(["aaa", "asd", "이뽀삐"]));
+    dispatch(setMatchingPosts(["asd", "asd", "Qwe"]));
+    dispatch(setCertificationPosts(["zzz", "qweqwe", "Zdfkshfka"]));
+  }, []);
+
   return (
     <MainPageComponent>
       <MainInfo />
-      <MainSection1 pets={["1", "2", "3", "4", "5", "6"]} />
-      <MainSection2 title="오늘의 매칭 글" contents={["ㅋㅋ", "ㅋㅋㅋ", "aaa"]} color="main" />
-      <MainSection2 title="따뜻한 후기" contents={["ㅋㅋ", "ㅋㅋㅋ", "aaa"]} isRightTitle={true} color="sub" />
+      <MainSection1 pets={pets} />
+      <MainSection2 title="오늘의 매칭 글" contents={matchingPosts} color="main" />
+      <MainSection2 title="따뜻한 후기" contents={certificationPosts} isRightTitle={true} color="sub" />
     </MainPageComponent>
   );
 }
