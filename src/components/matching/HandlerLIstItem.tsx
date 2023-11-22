be883e817cae53a8376682c15ab2574d1d25f9a7
yoonsoohwa <blue.yoonsh01@gmail.com>
2023-11-22 09:58:31 +0900

    Design: css style 변경

--- src/components/matching/HandlerLIstItem.tsx
+++ src/components/matching/HandlerLIstItem.tsx
@@ -4,31 +4,36 @@ import { Button } from "@mui/material";
 
 export function HandlerListItem() {
   return (
-    <ItemLayout>
-      <UserImg src={personImg} />
-      <UserInfo>
-        <span>쿵치팍치</span>
-        <span>서울특별시 종로구 효자동</span>
-      </UserInfo>
-      <Button variant="outlined" size="small" color="subW" sx={{minWidth: "fit-content"}}>
-        프로필
-      </Button>
-    </ItemLayout>
+    <ItemContainer>
+      <ItemLayout>
+        <UserImg src={personImg} />
+        <UserInfo>
+          <span>쿵치팍치</span>
+          <span>서울특별시 종로구 효자동</span>
+        </UserInfo>
+        <Button variant="outlined" size="small" color="subW" sx={{ minWidth: "fit-content" }}>
+          프로필
+        </Button>
+      </ItemLayout>
+    </ItemContainer>
   );
 }
+
+const ItemContainer = styled.div`
+  height: 3rem;
+  padding: 5px;
+  background-color: ${({ theme }) => theme.main4};
+  box-sizing: border-box;
+  &:hover {
+    background: #eaeaea;
+  }
+`;
 
 const ItemLayout = styled.div`
   display: flex;
   width: 100%;
-  height: 3rem;
+  height: 100%;
   align-items: center;
-  padding: 5px;
-  background-color: ${({theme}) => theme.main4};
-  box-sizing: border-box;
-
-  &:hover {
-    background: #eaeaea;
-  }
 `;
 
 const UserImg = styled.img`
--- src/components/matching/WalkDetailInfo.tsx
+++ src/components/matching/WalkDetailInfo.tsx
@@ -90,7 +90,7 @@ const WalkInfoItem = styled(TextAlignLayout)`
     font-size: 18px;
   }
   p {
-    font-weight: 500;
+    font-weight: 400;
     text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.25);
   }
 `;
