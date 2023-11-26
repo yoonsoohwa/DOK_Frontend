2ea1b44fce4525bd90d2c369c8b26c158caf4b64
yoonsoohwa <blue.yoonsh01@gmail.com>
2023-11-27 05:57:28 +0900

    Feat: 매칭페이지 무한 스크롤 적용 Feat: 매칭 카드별 상세페이지 연결

--- src/pages/MatchingList.tsx
+++ src/pages/MatchingList.tsx
@@ -7,28 +7,44 @@ import { MatchingCard } from "../components/matching/Card";
 import { ScrollToTopButton } from "../components/common/button/ScrollTopButton";
 import { ListPageTopBar } from "../components/common/list-page/ListPageTopBar";
 import { CardListContainer } from "../styles/CardListContainer";
+import { Link } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
 
 export function MatchingListPage() {
   const dispatch = useDispatch<AppDispatch>();
   const { matchingPosts } = useSelector((state: RootState) => state.matching);
 
+  const [scrollRef, inView] = useInView();
+
   const addPostList = async () => {
     const res = await fetch("/src/api/mock/matching-posts.json");
     const data = await res.json();
-    console.log(data);
     dispatch(addMatchingPosts(data));
   };
   useEffect(() => {
-    addPostList();
-  }, []);
+    if (inView) {
+      addPostList();
+    }
+  }, [inView]);
 
   return (
     <MatchingList>
       <MatchingBanner />
       <Section>
         <ListPageTopBar yellow="132" black="개의 매칭 요청이 있습니다." />
-        <CardListContainer>{Children.toArray(matchingPosts.map((post) => <MatchingCard post={post} />))}</CardListContainer>
+        <CardListContainer>
+          {Children.toArray(
+            matchingPosts.map((post) => {
+              return (
+                <Link to={`/matching/${post._id}`}>
+                  <MatchingCard post={post} />
+                </Link>
+              );
+            })
+          )}
+        </CardListContainer>
       </Section>
+      <div className="scroll-ref" ref={scrollRef}></div>
       <ScrollToTopButton />
     </MatchingList>
   );
