96e69e0113766324747e02deee477a84d553d915
LeeJinIe <jj074567@gmail.com>
2024-01-22 12:14:11 +0900

    Fix: 북마크를 통해 매칭글로 이동 시 북마크가 사라지는 에러 해결 - 에러 해결 - 인증 북마크에서 리팩토링 빠진부분 추가

--- src/components/header/MatchingBookmark.tsx
+++ src/components/header/MatchingBookmark.tsx
@@ -1,26 +1,22 @@
 import { Button, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
-import { WarningAmberRounded, WarningRounded } from '@mui/icons-material';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState, setMatchingBookmark } from 'store/index';
 import { Certification, TableContainer } from './MatchingBookmark.style';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
 import header_bookmark_off from '/svg/header_bookmark_off.svg';
 import header_bookmark_on from '/svg/header_bookmark_on.svg';
+import { bookmarkUrl } from 'api/apiUrls';
 
 export function MatchingBookmark() {
   const { user } = useSelector((state: RootState) => state.user);
   const { matchingBookmark } = useSelector((state: RootState) => state.matching) as any;
   const dispatch = useDispatch<AppDispatch>();
   const [openBox, setOpenBox] = useState(false);
-  const location = useLocation();
-  const navigate = useNavigate();
-
 
   // 작성한 매칭글 API 연동
   const getUserMachingList = async () => {
     try {
-      const res = await fetch(`api/bookMark/myMatchingPostInfo`, { credentials: 'include' });
+      const res = await fetch(`${bookmarkUrl}/myMatchingPostInfo`, { credentials: 'include' });
       const data = await res.json();
 
       if (res.ok) {
@@ -32,10 +28,6 @@ export function MatchingBookmark() {
       console.log('fetch error: ', e);
     }
   };
-
-  if (location.pathname.includes('/matching')) {
-    getUserMachingList();
-  }
 
   const handleClickIcon = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (!openBox) {
@@ -51,18 +43,17 @@ export function MatchingBookmark() {
   };
 
   useEffect(() => {
-    
     if (user._id) {
       getUserMachingList();
     }
-  }, [location.pathname, location.search, location.hash]);
+  }, []);
 
   return (
     <>
       {(user._id && !matchingBookmark.length) || (
-        <Certification>          
+        <Certification>
           <div onClick={handleClickIcon}>{openBox ? <img src={header_bookmark_off} className="icon" /> : <img src={header_bookmark_on} className="icon" />}</div>
-          {openBox && (          
+          {openBox && (
             <>
               <div className="hidden" onClick={handleClose}></div>
               <TableContainer>
@@ -74,34 +65,21 @@ export function MatchingBookmark() {
                     </TableRow>
                   </TableHead>
                   <TableBody>
-                    {matchingBookmark.map((post : any) =>                      
-
-                      post.map((item : any, index : any) => 
-                        (
-                          item.userDog?.dogName ? (
-                            <TableRow key={item._id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    {matchingBookmark.map((post: any) =>
+                      post.map((item: any, index: any) =>
+                        item.userDog?.dogName ? (
+                          <TableRow key={item._id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                             <TableCell component="th" scope="row">
-                            {item.userDog?.dogName} | {item.walkingDate?.split('T')[0]} | {item.walkingDuration*60}분
+                              {item.userDog?.dogName} | {item.walkingDate?.split('T')[0]} | {item.walkingDuration * 60}분
                             </TableCell>
-                            <TableCell align="right">                          
-                                <Button id={item._id} color="subW" variant="outlined" size="small">
-                                <a href={`/matching/${item._id}`}>
-                                  {matchingBookmark[1][index]}건의 요청
-                                </a>
-                                {/* <Link 
-                                  to={`/matching/${item._id}`}          
-                                  onClick={() => {
-                                    navigate(`/matching/${item._id}`);
-                                  }}                        
-                                >
-                                  {matchingBookmark[1][index]}건의 요청
-                                </Link> */}
-                                </Button>
+                            <TableCell align="right">
+                              <Button id={item._id} color="subW" variant="outlined" size="small">
+                                <a href={`/matching/${item._id}`}>{matchingBookmark[1][index]}건의 요청</a>
+                              </Button>
                             </TableCell>
                           </TableRow>
-                          ) : null                          
-                        )
-                      )
+                        ) : null,
+                      ),
                     )}
                   </TableBody>
                 </Table>
